4c1f66ff0f638f8f27c16943a0ea0f5c21e65563
Brendon Posen <brendon.posen@payfast.co.za>
2017-05-04 10:23:05 +0200

    update

--- include/plugins/jrPayment/PayFast.php
+++ include/plugins/jrPayment/PayFast.php
@@ -4,7 +4,9 @@
  * PayFast Jamroom Payments Plug in
  * @package Talldude_Library
  * @subpackage Payment_Plugins
- * @copyright 2013 PayFast (PTY) Ltd -- www.payfast.co.za
+ * Copyright (c) 2008 PayFast (Pty) Ltd
+ * You (being anyone who is not PayFast (Pty) Ltd) may download and use this plugin / code in your own website in conjunction with a registered and active PayFast account. If your PayFast account is terminated for any reason, you may not use this plugin / code or part thereof.
+ * Except as expressly indicated in this licence, you may not use, copy, modify or distribute this plugin / code or part thereof in any way.
  * @author Ron Darby -- ron.darby@payfast.co.za
  * @filesource
  * $Id: PayFast.php,v 1.0.0 2013/01/24 15:30:00 $
